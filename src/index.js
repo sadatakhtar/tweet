5591abaccb4ead4b4132b54c57deb1c7028e9c68
Sadat <sadat0akhtar@gmail.com>
2020-08-14 14:49:35 +0100

    created files

---
+++ src/index.js
@@ -0,0 +1,52 @@
+let charLimit = 280;
+let user = "@User123";
+let imgTag;
+
+
+//SELECTORS
+let submitButton = document.getElementById('submitBtn');
+let tweetOutput = document.querySelector('.tweetOutput');
+let textArea = document.getElementById('myTextArea');
+let counterDiv = document.querySelector('.counter');
+
+//EVENT LISTENERS
+submitButton.addEventListener('click', function(e){
+    e.preventDefault();
+    let newParagraphTag = document.createElement('p');
+    imgTag = document.createElement('img');
+    newParagraphTag.id="tweetP";
+    imgTag.id="icon";
+    imgTag.src = "./images/delete.svg";
+    
+    if(textArea.value.length <= charLimit){
+        newParagraphTag.innerText = `${user}- ${textArea.value}`;
+        tweetOutput.appendChild(newParagraphTag);
+        newParagraphTag.appendChild(imgTag);
+    }else{
+        document.getElementById('cc').style.color="red";
+    }
+
+    textArea.value = "";
+    setTimeout(resetCharCounter, 1000);
+
+    //EVENT LISTENER FOR DELETE ICON
+    imgTag.onclick = () =>{
+        newParagraphTag.innerText = "";
+    }
+});
+//RESET COUNTER 
+function resetCharCounter(){
+    document.getElementById('newCCParagraph').innerHTML = `0 characters`;
+    document.getElementById('newCCParagraph').style.color="black";
+};
+//CHECK CHARACTER LIMIT AND HIGHLIGHT COLOR
+function cc(obj){
+    if(obj.value.length > charLimit){
+        document.getElementById('newCCParagraph').innerHTML = obj.value.length+ ' characters';
+        document.getElementById('newCCParagraph').style.color = "red";
+    }else {
+        document.getElementById('newCCParagraph').innerHTML = obj.value.length+ ' characters';
+        document.getElementById('newCCParagraph').style.color = "green";
+    }
+    
+}
